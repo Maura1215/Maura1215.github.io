bde31adfacd7c6d053588121eff83e32ccfcff9b
Maura1215 <mirarchi@susqu.edu>
2016-02-28 14:56:56 -0500

    add more styling and java

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload = function(e) {
+	function turtleFact () {
+	alert("Some turtles can live longer than humans");
+}
+
+turtleFact ();
+
+function temptGang(argument){
+alert(gang name + "Come out and play!")
+}
+
+function addnumbers(num1, num2)
+{alert(num1+num2)
+}
+
+addumbers(10, 5)
+
+function square(number) {
+	return number * number;
+}
+
+function displaySquare(squareNumber) {
+	console.log("Your number is" + squareNumber);
+}
+
+displaySquare(sqaure(3));
+
